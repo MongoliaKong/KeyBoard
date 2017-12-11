adcfdc4fe9a81451c685f8df0efcce7f26e32877
kongmongolia@gmail.com <kongmongolia@gmail.com>
2017-12-11 15:54:57 +0800

    add test

---
+++ frameWORKD.framework/Headers/frameWORKD.h
@@ -0,0 +1,19 @@
+//
+//  frameWORKD.h
+//  frameWORKD
+//
+//  Created by user on 2017/12/11.
+//  Copyright © 2017年 iwall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for frameWORKD.
+FOUNDATION_EXPORT double frameWORKDVersionNumber;
+
+//! Project version string for frameWORKD.
+FOUNDATION_EXPORT const unsigned char frameWORKDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <frameWORKD/PublicHeader.h>
+
+#import <frameWORKD/SWT.h>
